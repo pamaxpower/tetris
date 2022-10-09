3bebe59a566938ddb05efe817a2fd29255765578
lordIK <jetlord@mail.ru>
2022-10-09 19:21:14 +0300

    1-Turn

---
+++ 1-Turn/Program.cs
@@ -0,0 +1,80 @@
+﻿// Повоторот массива
+
+Console.Clear();
+
+void PrintArray (int [,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            if (inputArray[i,j] == 1) Console.Write("# ");
+            else Console.Write(". ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] TurnArrayRigth (int[,] arr)
+{
+    int[,] turnArrayRigth = new int[arr.GetLength(0), arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+        turnArrayRigth[i, j] = arr[arr.GetLength(0)-j-1, i];
+        }
+    }
+    return turnArrayRigth;
+}
+
+int[,] TurnArrayLeft (int[,] arr)
+{
+    int[,] turnArrayLeft = new int[arr.GetLength(0), arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+        turnArrayLeft[i, j] = arr[j,arr.GetLength(0)-1-i];
+        }
+    }
+    return turnArrayLeft;
+}
+
+
+int[,] array = new int [3,3] {
+{ 1,1,0 },
+{ 1,0,0 },
+{ 1,0,0 }
+};
+
+PrintArray(array);
+
+string exit = "n";
+while (exit == "n")
+{
+Console.WriteLine();
+Console.WriteLine("Нажмите d, чтобы повернуть фигуру вправо и a, чтобы повернуть влево");
+string next = Console.ReadLine();
+
+
+
+    if (next == "d")
+    {
+        TurnArrayRigth(array);
+        PrintArray(TurnArrayRigth(array));
+        array = TurnArrayRigth(array);
+    }
+    if (next == "a")
+    {
+        TurnArrayLeft(array);
+        PrintArray(TurnArrayLeft(array));
+        array = TurnArrayLeft(array);
+    }
+
+
+Console.WriteLine();
+Console.Write("Выйти? (y/n): ");
+exit = Console.ReadLine();
+}
+
---
+++ test/Program.cs
@@ -0,0 +1,59 @@
+﻿// int[,] array = new int[4,4] {
+//     { 1,2,3,4 },
+//     { 5,6,7,8 },
+//     { 9,0,1,2 },
+//     { 3,4,5,6 }
+// };
+
+Console.Clear();
+
+int[,] array = FillArray(3, 3, 1 , 9);
+PrintArray(array);
+Console.WriteLine();
+
+int[,] rotated = RotateMatrix(array, 3);
+
+PrintArray(rotated); 
+
+static int[,] RotateMatrix(int[,] matrix, int n)
+{
+    int[,] ret = new int[n, n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            ret[i, j] = matrix[n - j - 1, i];
+        }
+    }
+
+    return ret;
+}
+
+int[,] FillArray  (int rows, int columns, int min, int max)
+{
+    int[,] filledArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            filledArray[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray (int [,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(" " + inputArray[i,j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
