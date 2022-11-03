9e7e27baacd7bbb4d8b79bbc0f31d6f43748a7bb
lordIK <jetlord@mail.ru>
2022-11-03 18:25:33 +0300

    fff

--- Rec/Program.cs
+++ Rec/Program.cs
@@ -4,13 +4,12 @@ M = 1; N = 5 -> 2, 4
 M = 4; N = 8 -> 4, 6, 8
 */
 
-int a = 1071;
-int b = 462;
+// int a = 1071;
+// int b = 462;
 
-while (a % b != 0)
-{
-    int c = a % b;
-    Console.WriteLine(c);
-    a = c;
-}
-
+// while (a % b != 0)
+// {
+//     int c = a % b;
+//     Console.WriteLine(c);
+//     a = c;
+// }
